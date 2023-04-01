0462a791d48fab4c0d850d36cf153c7f72d6344b
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2023-04-01 15:55:19 +0530

    Added functions to compress and uncompress a chunk of memory

--- core_utils/ev_compression.h
+++ core_utils/ev_compression.h
@@ -1,6 +1,8 @@
 #ifndef COMPRESSION_H_INCLUDED
 #define COMPRESSION_H_INCLUDED
 
+#include "zlib.h"
+
 __BEGIN_DECLS
 
 extern int compress_inp_buf(void *in, size_t in_size, void **out_ptr, size_t* written_size_ptr);
